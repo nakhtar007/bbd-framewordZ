23e78137bddcea2b24bb1ca2481dddcb68b700d9
nakhtar007 <zafar.asar@tekschool.us>
2023-02-27 12:58:28 -0600

    this is first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Zafar
+ *
+ */
+module javaNewProject {
+}
---
+++ src/newbbdFrameWork/FirstClassForRepo.java
@@ -0,0 +1,16 @@
+package newbbdFrameWork;
+
+public class FirstClassForRepo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(sum(2,4));
+
+	}
+	
+	public static int sum(int a, int b) {
+		return a+b;
+	}
+
+}
